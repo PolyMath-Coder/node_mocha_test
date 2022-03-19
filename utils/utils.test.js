2c0d5266c4f905dc419036a0b72f72d2c0dd263f
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-03-19 15:59:51 +0100

    initial commit

---
+++ utils/utils.test.js
@@ -0,0 +1,20 @@
+var expect = require('expect');
+var createSpy = expect.createSpy;
+var spyOn = expect.spyOn;
+var isSpy = expect.isSpy;
+
+const utils = require('./utils');
+
+it('should add two numbers correctly', () => {
+  var res = utils.add(4, 9);
+
+  expect(res).toBe(13);
+});
+
+it('should take the square of the number', () => {
+  var res = utils.square(8);
+
+  if (res !== 64) {
+    throw new Error(`Expected 64, but actually got ${res}`);
+  }
+});
